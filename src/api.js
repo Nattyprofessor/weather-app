00dae50ba199b0884cca6324194418519b7b72cc
Anthony Kabuthu <kabuthuantony@gmail.com>
2024-11-13 04:21:19 +0300

    Project Updates

---
+++ src/api.js
@@ -0,0 +1,13 @@
+
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '65ca883c77mshd92e7f94105353fp1a3bc4jsncdd8f80c3284',
+		'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL ="https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "d4c86f05a967edbe8bfdf9e4c7fe5aa1";
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,35 @@
+import { useRef } from "react";
+import { Link } from 'react-router-dom';
+import { FaBars, FaTimes } from "react-icons/fa";
+import "./main.css";
+
+function Navbar() {
+  const navRef = useRef();
+
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+  };
+
+  return (
+    <header>
+      <h3>WEATHER APP</h3>
+      <nav ref={navRef}>
+        <Link to="/weather">Check Weather</Link>
+        <Link to="/about">About</Link>
+        <Link to="/login">Login</Link>
+        <button
+          className="nav-btn nav-close-btn"
+          onClick={showNavbar}>
+          <FaTimes />
+        </button>
+      </nav>
+      <button
+        className="nav-btn"
+        onClick={showNavbar}>
+        <FaBars />
+      </button>
+    </header>
+  );
+}
+
+export default Navbar;
