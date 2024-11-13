771239d13b8db7f1b96141713348e3180daff822
Anthony Kabuthu <kabuthuantony@gmail.com>
2024-11-13 23:16:47 +0300

    Deploy

--- src/components/current-weather/current-weather.js
+++ src/components/current-weather/current-weather.js
@@ -8,7 +8,8 @@ const CurrentWeather = ({data}) => {
           <p className="city">{data.city}</p>
           <p className="weather-description">{data.weather[0].description}</p>
         </div>
-        <img alt="weather" className="weather-icon" src={`icons/${data.weather[0].icon}.png`}></img>
+        <img alt="weather" className="weather-icon" src={`${process.env.PUBLIC_URL}/icons/${data.weather[0].icon}.png`}
+        ></img>
       </div>
       <div className="bottom">
         <p className="temperature">{Math.round(data.main.temp)}°C</p>
